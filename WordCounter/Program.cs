ef0976dae8f21ed5c6d16000c9b656af01657e27
Pandiannpn <pandian.n-p-n@capgemini.com>
2026-03-17 12:20:00 +0530

    Add files via upload
    Initial commit

---
+++ WordCounter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Pandian\GitHubCopilotTraining\Sample .NetApp\SampleWebApp\WordCounter\input.txt";
+        if (File.Exists(filePath))
+        {
+            string text = File.ReadAllText(filePath);
+            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Number of words: {words.Length}");
+         // Update this connection string with your actual Postgres host/port/credentials
+        string connectionString = "Host=localhost;Port=5432;Database=Powerhouse;Username=postgres;Password=admin";
+
+        var repo = new WordCountRepository(connectionString);
+        repo.SaveWordCount(Path.GetFileName(filePath), words.Length);
+
+        Console.WriteLine("Saved word count to database.");   
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
+        }
+    }
+}
